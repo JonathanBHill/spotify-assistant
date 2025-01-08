8fcc3f626476338b2cc939b7ebd30efbbb463047
Jonathan Hill <jonathans-github@pm.me>
2025-01-08 05:18:05 -0700

    Version: 0.3.0 Date: 01/06/25 Author: Jonathan Hill Commit type: Feature Commit scope: CLI Commit subject: Adds CLI functionality

--- spotify-assistant/src/main.rs
+++ spotify-assistant/src/main.rs
@@ -1,19 +1,24 @@
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
+use spotify_assistant_cli::interface::TerminalApp;
+// use spotify_assistant_core::actions::update::PlaylistCreator;
+// use spotify_assistant_core::traits::apis::Querying;
 use spotify_assistant_core::utilities::configurations::CustomFormatter;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     #[cfg(debug_assertions)]
     {
         let subscriber = FmtSubscriber::builder()
             .event_format(CustomFormatter)
-            .with_max_level(Level::INFO)
+            .with_max_level(Level::TRACE)
             .finish();
         // Initialize the global tracing subscriber
         tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
         let span = tracing::span!(Level::INFO, "main");
         let _enter = span.enter();
     }
-    println!("Thanks for checking out the Spotify Assistant!");
+    let app = TerminalApp::new();
+    app.run().await;
 }
