053f5af3db601a439a6ad2f849d5919e04fb0836
Jonathan Hill <jonathans-github@pm.me>
2025-10-21 06:52:55 -0600

    Version: 0.3.13 Date: 10/21/25 Author: Jonathan Hill Commit type: Refactor Commit scope: Core Commit subject: Refactors tracing subscriber instantiation to be more modular

--- spotify-assistant-core/src/utilities/mod.rs
+++ spotify-assistant-core/src/utilities/mod.rs
@@ -2,3 +2,4 @@ pub mod configurations;
 pub mod filesystem;
 pub mod datetime;
 pub mod general;
+pub mod logging;
--- spotify-assistant/src/bin/update_release_radar.rs
+++ spotify-assistant/src/bin/update_release_radar.rs
@@ -1,23 +1,13 @@
 use tracing::Level;
-use tracing_subscriber::FmtSubscriber;
 
 use spotify_assistant_core::actions::update::Editor;
-use spotify_assistant_core::traits::apis::Querying;
-use spotify_assistant_core::utilities::configurations::CustomFormatter;
+use spotify_assistant_core::utilities::logging::init_tracing;
 
 #[tokio::main]
 async fn main() {
-    #[cfg(debug_assertions)]
-    {
-        let subscriber = FmtSubscriber::builder()
-            .event_format(CustomFormatter)
-            .with_max_level(Level::TRACE)
-            .finish();
-        // Initialize the global tracing subscriber
-        tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-        let span = tracing::span!(Level::INFO, "main");
-        let _enter = span.enter();
-    }
+    init_tracing();
+    let span = tracing::span!(Level::INFO, "main");
+    let _enter = span.enter();
     let my_release_radar = Editor::release_radar().await;
     my_release_radar.update_playlist().await
 }
