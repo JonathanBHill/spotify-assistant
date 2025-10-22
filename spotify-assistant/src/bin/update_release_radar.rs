52a43dc34308a7cf887d4f6428822703206e88aa
Jonathan Hill <jonathans-github@pm.me>
2025-10-22 07:00:51 -0600

    Version: 0.3.20 Date: 10/22/25 Author: Jonathan Hill Commit type: Refactor Commit scope: Binaries Commit subject: Refactors binaries to instantiate the custom subscriber with function

--- spotify-assistant/src/bin/update_release_radar.rs
+++ spotify-assistant/src/bin/update_release_radar.rs
@@ -8,6 +8,7 @@ async fn main() {
     init_tracing();
     let span = tracing::span!(Level::INFO, "main");
     let _enter = span.enter();
+
     let my_release_radar = Editor::release_radar().await;
     my_release_radar.update_playlist().await
 }
--- spotify-assistant/src/main.rs
+++ spotify-assistant/src/main.rs
@@ -4,6 +4,9 @@ use spotify_assistant_core::utilities::logging::init_tracing;
 #[tokio::main]
 async fn main() {
     init_tracing();
+    let span = tracing::span!(tracing::Level::INFO, "main");
+    let _enter = span.enter();
+    
     let app = TerminalApp::new();
     app.run().await;
 }
